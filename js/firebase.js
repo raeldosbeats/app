0adc96077911f04c2ef176f49d5fbed029d51bc1
Israel Rodrigues Moura <99314565+raeldosbeats@users.noreply.github.com>
2024-07-07 23:53:13 -0300

    update files

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,11 @@
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyB8gBNpyPf3qQCmMCPqsEaBjeM4H-iTSKI",
-    authDomain: "beatstore-81370.firebaseapp.com",
-    projectId: "beatstore-81370",
-    storageBucket: "beatstore-81370.appspot.com",
-    messagingSenderId: "647809361673",
-    appId: "1:647809361673:web:ae7069bbdd4f51582715d8"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   };
 
   // Inicialize o Firebase
